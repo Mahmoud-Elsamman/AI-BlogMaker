b09f3af91d6684ba437f84fb02d24e03ae242d0e
Your Name <you@example.com>
2023-05-03 05:40:51 +0200

    Set up auth0 for authentication

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div />;
+  return <div>
+    <h1>This is the home page</h1>
+    <div>
+      <Link href='/api/auth/login'>
+        Login
+      </Link>
+    </div>
+  </div>;
 }

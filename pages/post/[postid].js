9c077325cd7dc68902bb84baf235958f005a5733
Your Name <you@example.com>
2023-05-11 16:14:08 +0200

    get the specific post from the database

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,6 +1,10 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+import { AppLayout } from "../../components/AppLayout";
 
-export default function Post() {
+export default function Post(props) {
+  console.log(props);
   return (
     <div>
       <h1>This is the the post page</h1>
@@ -8,8 +12,41 @@ export default function Post() {
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+Post.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("AI-BlogMaker");
+
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
 });

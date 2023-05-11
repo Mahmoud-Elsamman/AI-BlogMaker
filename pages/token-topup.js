8bc0f3a1a77d87fab84e3c0962d3ba78b6ee336c
Your Name <you@example.com>
2023-05-11 16:01:26 +0200

    add the addTokens api call to token-topup page

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -2,9 +2,17 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout";
 
 export default function TokenPage() {
+  const handleClick = async () => {
+    await fetch("/api/addTokens", {
+      method: "POST",
+    });
+  };
   return (
     <div>
       <h1>This is the the token page</h1>
+      <button className='btn' onClick={handleClick}>
+        Add Tokens
+      </button>
     </div>
   );
 }

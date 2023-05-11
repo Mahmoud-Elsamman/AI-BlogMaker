c6addbb73687ff6fbeb07e2ae99c201c6789f4fb
Your Name <you@example.com>
2023-05-11 15:47:55 +0200

    create the addToken endpoint

---
+++ pages/api/addTokens.js
@@ -0,0 +1,29 @@
+import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const { user } = await getSession(req, res);
+  console.log("user", user);
+
+  const client = await clientPromise;
+  const db = client.db("AI-BlogMaker");
+
+  const userProfile = await db.collection("users").updateOne(
+    {
+      auth0Id: user.sub,
+    },
+    {
+      $inc: {
+        availableTokens: 10,
+      },
+      $setOnInsert: {
+        auth0Id: user.sub,
+      },
+    },
+    {
+      upsert: true,
+    }
+  );
+
+  res.status(200).json({ name: "John Doe" });
+}

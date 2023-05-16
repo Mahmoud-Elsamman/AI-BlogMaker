ea14874c565f7d22c3944797a99f958176295326
Your Name <you@example.com>
2023-05-16 16:15:28 +0200

    render the post content for a particular post page

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -2,12 +2,37 @@ import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
 import { AppLayout } from "../../components/AppLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { getAppProps } from "../../utils/getAppProps";
 
 export default function Post(props) {
   console.log(props);
   return (
-    <div>
-      <h1>This is the the post page</h1>
+    <div className='overflow-auto h-full'>
+      <div className='max-w-screen-sm mx-auto'>
+        <div className='text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm'>
+          SEO title and meta description
+        </div>
+        <div className='p-4 my-2 border border-stone-200 rounded-md'>
+          <div className='text-blue-600 text-2xl font-bold'>{props.title}</div>
+          <div className='mt-2'>{props.metaDescription}</div>
+        </div>
+        <div className='text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm'>
+          Keywords
+        </div>
+        <div className='flex flex-wrap pt-2 gap-1'>
+          {props.keywords.split(",").map((keyword, i) => (
+            <div key={i} className='p-2 rounded-full bg-slate-800 text-white'>
+              <FontAwesomeIcon icon={faHashtag} /> {keyword}
+            </div>
+          ))}
+        </div>
+        <div className='text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm'>
+          Blog post
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: props.postContent || "" }} />
+      </div>
     </div>
   );
 }
@@ -18,6 +43,7 @@ Post.getLayout = function getLayout(page, pageProps) {
 
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
+    const props = await getAppProps(ctx);
     const userSession = await getSession(ctx.req, ctx.res);
     const client = await clientPromise;
     const db = client.db("AI-BlogMaker");
@@ -46,6 +72,7 @@ export const getServerSideProps = withPageAuthRequired({
         title: post.title,
         metaDescription: post.metaDescription,
         keywords: post.keywords,
+        ...props,
       },
     };
   },

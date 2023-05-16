393889c434ca968ee955de6ce765cff73b8bd9fc
Your Name <you@example.com>
2023-05-16 16:53:28 +0200

    redirect the user to the success page

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,11 +1,18 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout";
+import { getAppProps } from "../utils/getAppProps";
 
 export default function TokenPage() {
   const handleClick = async () => {
-    await fetch("/api/addTokens", {
+    const result = await fetch("/api/addTokens", {
       method: "POST",
     });
+
+    const json = await result.json();
+
+    console.log("RESULT: ", json);
+
+    window.location.href = json.session.url;
   };
   return (
     <div>
@@ -21,8 +28,11 @@ TokenPage.getLayout = function getLayout(page, pageProps) {
   return <AppLayout {...pageProps}>{page}</AppLayout>;
 };
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const props = await getAppProps(ctx);
+    return {
+      props,
+    };
+  },
 });

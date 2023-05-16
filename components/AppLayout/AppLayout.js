3bf6ec3b80b98483627637023efee4cea7cb3370
Your Name <you@example.com>
2023-05-16 16:12:52 +0200

    render list of user available posts in the sidebar

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { Logo } from "../Logo";
 
-export const AppLayout = ({ children }) => {
+export const AppLayout = ({ children, availableTokens, posts, postId }) => {
   const { user } = useUser();
 
   return (
@@ -14,15 +14,25 @@ export const AppLayout = ({ children }) => {
         <div className='bg-slate-800 px-2'>
           <Logo />
           <Link href='/post/new' className='btn'>
-            New Post
+            New post
           </Link>
           <Link href='/token-topup' className='block mt-2 text-center'>
             <FontAwesomeIcon icon={faCoins} className='text-yellow-500' />
-            <span className='pl-1'>0 tokens available</span>
+            <span className='pl-1'>{availableTokens} tokens available</span>
           </Link>
         </div>
-        <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>
-          list of posts
+        <div className='px-4 flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>
+          {posts.map((post) => (
+            <Link
+              key={post._id}
+              href={`/post/${post._id}`}
+              className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${
+                postId === post._id ? "bg-white/20 border-white" : ""
+              }`}
+            >
+              {post.topic}
+            </Link>
+          ))}
         </div>
         <div className='bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2'>
           {!!user ? (
@@ -31,10 +41,10 @@ export const AppLayout = ({ children }) => {
                 <Image
                   src={user.picture}
                   alt={user.name}
-                  width={50}
                   height={50}
+                  width={50}
                   className='rounded-full'
-                ></Image>
+                />
               </div>
               <div className='flex-1'>
                 <div className='font-bold'>{user.email}</div>
@@ -48,7 +58,7 @@ export const AppLayout = ({ children }) => {
           )}
         </div>
       </div>
-      <div>{children}</div>
+      {children}
     </div>
   );
 };

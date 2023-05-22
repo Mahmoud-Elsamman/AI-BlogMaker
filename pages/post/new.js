6697908fbd6b51d13c4df774f32993355bd86cb6
Your Name <you@example.com>
2023-05-22 21:35:44 +0200

    add the loading logo and form validation

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,4 +1,6 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { AppLayout } from "../../components/AppLayout";
@@ -8,53 +10,82 @@ export default function NewPost(props) {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [generating, setGenerating] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`/api/generatePost`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ topic, keywords }),
-    });
+    setGenerating(true);
+    try {
+      const response = await fetch(`/api/generatePost`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ topic, keywords }),
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    console.log("Result: ", json);
+      console.log("Result: ", json);
 
-    if (json?.postId) {
-      router.push(`/post/${json.postId}`);
+      if (json?.postId) {
+        router.push(`/post/${json.postId}`);
+      }
+    } catch (error) {
+      setGenerating(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <strong>Generate a blog post on the topic of:</strong>
-          </label>
-          <textarea
-            className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
-            value={topic}
-            onChange={(e) => setTopic(e.target.value)}
-          />
+    <div className='h-full overflow-hidden'>
+      {!!generating && (
+        <div className='text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center'>
+          <FontAwesomeIcon icon={faBrain} className='text-8xl' />
+          <h6>Generating...</h6>
         </div>
-        <div>
-          <label>
-            <strong>Targeting the following keywords:</strong>
-          </label>
-          <textarea
-            className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
-            value={keywords}
-            onChange={(e) => setKeywords(e.target.value)}
-          />
+      )}
+
+      {!generating && (
+        <div className='w-full h-full flex flex-col overflow-auto'>
+          <form
+            onSubmit={handleSubmit}
+            className='m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded=md shadow-xl border border-slate-200 shadow-slate-200'
+          >
+            <div>
+              <label>
+                <strong>Generate a blog post on the topic of:</strong>
+              </label>
+              <textarea
+                className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+                maxLength={80}
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Targeting the following keywords:</strong>
+              </label>
+              <textarea
+                className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+                maxLength={80}
+              />
+              <small className='block mb-2'>
+                Separate keywords with a comma
+              </small>
+            </div>
+            <button
+              type='submit'
+              className='btn'
+              disabled={!topic.trim() || !keywords.trim()}
+            >
+              Generate
+            </button>
+          </form>
         </div>
-        <button type='submit' className='btn'>
-          Generate
-        </button>
-      </form>
+      )}
     </div>
   );
 }
@@ -66,6 +97,15 @@ NewPost.getLayout = function getLayout(page, pageProps) {
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
     const props = await getAppProps(ctx);
+
+    if (!props.availableTokens) {
+      return {
+        redirect: {
+          destination: "/token-topup",
+          permanent: false,
+        },
+      };
+    }
     return {
       props,
     };

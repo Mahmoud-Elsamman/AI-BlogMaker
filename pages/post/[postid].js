70a17b18cbca3a85b0b64f94aa8ef21110491d1b
Your Name <you@example.com>
2023-05-22 22:00:28 +0200

    add the ui to handle delete and confirm delete

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -5,9 +5,35 @@ import { AppLayout } from "../../components/AppLayout";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 import { getAppProps } from "../../utils/getAppProps";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import PostsContext from "../../context/postsContext";
 
 export default function Post(props) {
   console.log(props);
+  const router = useRouter();
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+  const { deletePost } = useContext(PostsContext);
+
+  const handleDeleteConfirm = async () => {
+    try {
+      const response = await fetch("/api/deletePost", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ postId: props.id }),
+      });
+
+      const json = await response.json();
+
+      if (json.success) {
+        deletePost(props.id);
+        router.replace("/post/new");
+      }
+    } catch (error) {}
+  };
+
   return (
     <div className='overflow-auto h-full'>
       <div className='max-w-screen-sm mx-auto'>
@@ -32,6 +58,39 @@ export default function Post(props) {
           Blog post
         </div>
         <div dangerouslySetInnerHTML={{ __html: props.postContent || "" }} />
+        <div className='my-4'>
+          {!showDeleteConfirm && (
+            <button
+              className='btn bg-red-600 hover:bg-red-700'
+              onClick={() => setShowDeleteConfirm(true)}
+            >
+              DELETE POST
+            </button>
+          )}
+
+          {!!showDeleteConfirm && (
+            <div>
+              <p className='p-2 bg-red-300 text-center'>
+                Are you sure you want to delete this post? This action is
+                irreversible
+              </p>
+              <div className='grid grid-cols-2 gap-2'>
+                <button
+                  onClick={() => setShowDeleteConfirm(false)}
+                  className='btn bg-stone-600 hover:bg-stone-700'
+                >
+                  cancel
+                </button>
+                <button
+                  onClick={handleDeleteConfirm}
+                  className='btn bg-red-600 hover:bg-red-700'
+                >
+                  confirm delete
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
@@ -68,10 +127,12 @@ export const getServerSideProps = withPageAuthRequired({
 
     return {
       props: {
+        id: ctx.params.postId,
         postContent: post.postContent,
         title: post.title,
         metaDescription: post.metaDescription,
         keywords: post.keywords,
+        postCreated: post.created.toString(),
         ...props,
       },
     };
